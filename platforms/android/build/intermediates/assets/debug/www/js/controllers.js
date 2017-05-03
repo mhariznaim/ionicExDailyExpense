021a94acaabe47ec34b56c92a97eb474c5cccb6f
mhariznaim <mhariznaim@gmail.com>
2017-05-03 10:26:35 +0800

    add platforms and plugin folders

---
+++ platforms/android/build/intermediates/assets/debug/www/js/controllers.js
@@ -0,0 +1,65 @@
+angular.module('app.controllers', ['ionic','ngCordova'])
+  
+.controller('addNewExpensesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $cordovaSQLite) {
+
+ //$("#expDate").datepicker({ defaultDate: new Date() });
+ 
+ 
+ 
+ $("#addNewExpenses-button1").click( function(){ 
+		
+		var strExpname = $("#expName").val();
+		var fltPrice = $("#expPrice").val();
+		var strDate = $("#expDate").val();
+		var strTime =$("#expTime").val();
+		
+		
+		
+		var strSqlSave = "insert into expenses(exp_name,exp_price,exp_date,exp_time) values (?,?,?,?)";
+		
+		myDB.transaction(function(transaction){
+		transaction.executeSql(strSqlSave,[strExpname,fltPrice,strDate,strTime], function(tx,result ){
+			alert("You have spend RM" + fltPrice +" for "+ strExpname + " on "+strDate + " " + strTime);
+		},
+		  function(error){
+		  alert("Noooo! Cannot Save!.."+ error);
+		  });
+		});
+ });
+
+}])
+   
+.controller('listOfExpensesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+	$("#getList").click(function(){
+	
+		varSelectAll = "select * from expenses;";
+		myDB.transaction(function(transaction){
+		transaction.executeSql(varSelectAll,[], function(tx,result ){
+			alert("Yeay! Data from sqlite: " + result.rows.item(0).strExpname);
+		},
+		  function(error){
+		  alert("Noooo! Cannot Save!.."+ error);
+		  });
+		});
+		
+	});
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+
+
+}])
+ 
